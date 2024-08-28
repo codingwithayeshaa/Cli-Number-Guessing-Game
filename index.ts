d8b6a1164f5a35362484a4678627d5d8541487ee
AyeshaKanwal <amnakanwal765@gmail.com>
2024-08-29 04:36:38 +0500

    Adding a number-guessing game.

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+console.log("\n\tWelcome to Number Guessing Game\n");
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6:",
+    }
+])
+
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congratulations! You guessed the correct number");
+}else{
+    console.log("Sorry, you guessed the wrong number");
+};
+
+
